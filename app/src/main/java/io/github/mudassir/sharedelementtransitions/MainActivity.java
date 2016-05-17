da3282d9a003907270420305db48087c23050fa5
mudassir <mudassir@users.noreply.github.com>
2016-05-17 11:44:56 -0400

    Added Retrolambda and Glide libraries

--- app/src/main/java/io/github/mudassir/sharedelementtransitions/MainActivity.java
+++ app/src/main/java/io/github/mudassir/sharedelementtransitions/MainActivity.java
@@ -2,6 +2,10 @@ package io.github.mudassir.sharedelementtransitions;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		findViewById(R.id.btn_two_activities).setOnClickListener(view -> Toast.makeText(view.getContext(), "test", Toast.LENGTH_SHORT).show());
+
+		Glide.with(this)
+				.load("https://raw.githubusercontent.com/mudassir/shared-element-transitions/master/img/android.png")
+				.into((ImageView) findViewById(R.id.img));
 	}
 }
